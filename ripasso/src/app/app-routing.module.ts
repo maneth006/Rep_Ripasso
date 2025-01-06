4b12e0c340e4251e919349ee158a3d19e06dfb23
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2025-01-06 18:56:04 +0000

    casso non riesco a finirlo

--- ripasso/src/app/app-routing.module.ts
+++ ripasso/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonlistComponent } from './pokemonlist/pokemonlist.component';
 import { HomeComponent } from './home/home.component';
-
+import { PokemondetailsComponent } from './pokemondetails/pokemondetails.component';
 const routes: Routes = [
   { path: 'home/:tipo', component: PokemonlistComponent },
   { path: 'home', component: HomeComponent },
+  {path: 'pokemon/:nome', component: PokemondetailsComponent},
   {path: '', redirectTo :'/home', pathMatch:'full'}
 ];
 
--- ripasso/src/app/pokemondetails/pokemondetails.component.ts
+++ ripasso/src/app/pokemondetails/pokemondetails.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { pokeDetail } from '../moduls/pokemondetails.moduls';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-pokemondetails',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemondetails.component.css']
 })
 export class PokemondetailsComponent {
+  o! : Observable<pokeDetail>
+  pokedata! : pokeDetail
+  loading! : boolean
+  url = 'https://pokeapi.co/api/v2/pokemon/'
 
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+        this.loading = true; 
+        this.route.paramMap.subscribe(this.getRouterParam);
+  }
+
+  getRouterParam = (params: ParamMap) =>
+  {
+    this.o = this.http.get<pokeDetail>(this.url + params.get('nome'))
+    this.o.subscribe(this.getData)
+  }
+    getData = (data : pokeDetail) =>
+    {
+      this.loading= false
+      this.pokedata = data
+    }
 }
